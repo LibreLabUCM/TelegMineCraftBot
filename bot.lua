3e1e2ae46578473aa4ca428e77eb68a436053e2e
DevPGSV <DevPGSV@gmail.com>
2015-07-28 20:29:10 +0200

    Recoded. Tested (works)

--- bot.lua
+++ bot.lua
@@ -1,33 +1,39 @@
 
 -- Config
-
-local token = "Token"
--- Telegram bot token (from @BotFather )
-local admins = {
- -- Telegram ID's of admin users
+local bot_path = "/custom/bot" -- Without "bot.lua", not ending in "/"
+local token = "Token" -- Telegram bot token (from @BotFather )
+local admins = { -- Telegram ID's of admin users
    12345678,
    87654321
 }
-local monitor_side = "left"
--- Where is the monitor? left, right?
+local monitor_side = "left" -- Where is the monitor? "left", "right"? If no monitor, use: "none"
 
 
 -- Set Up
-
---os.loadAPI("lib/json")
-local monitor = peripheral.wrap(monitor_side)
+os.loadAPI(bot_path .. "/lib/json")
 local api_url = "https://api.telegram.org/bot" .. token .. "/"
+if mon ~= "none" then
+  local mon = peripheral.wrap(monitor_side)
+  term.redirect(mon)
+end
+term.clear()
+term.setCursorPos(1, 1)
 
 
 -- Bot starts here:
-
- monitor.write("Starting up....")
-
+print("Starting up....")
 local request = http.get(api_url .. "getMe")
-monitor.write(request.readAll())
-request.close()
-
-
-
-
-
+if request ~= nil then
+  local jsonResult = json.decode(request.readAll())
+  if jsonResult["ok"] == true then
+    result = jsonResult["result"]
+    print("User: " .. result["username"])
+    print("Name: " .. result["first_name"])
+    print("ID: " .. result["id"])
+  else
+    print("Error!")
+  end
+  request.close()
+else
+  print("Error!")
+end
